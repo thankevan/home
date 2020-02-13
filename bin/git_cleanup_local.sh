100a31b1b83ce6fb061ec9a7ba13349262bb5164
thankevan <thankevan@gmail.com>
2020-02-13 10:56:23 -0500

    Add better UX to git_cleanup_local

--- bin/git_cleanup_local.sh
+++ bin/git_cleanup_local.sh
@@ -11,6 +11,7 @@ git pull
 echo ""
 echo "Going to delete:"
 git branch --merged | grep -v master
+echo "Hit enter to continue or ^-c to cancel"
 read foo
 
 git branch -d `git branch --merged | grep -v master` 2>/dev/null
