2ac8c87a5d55ec630610b238bb8e0f9d804cf6ba
thankevan <thankevan@gmail.com>
2020-06-25 16:23:40 -0400

    Fix completion so it works at any argument position

--- bin/bash_function_completion.sh
+++ bin/bash_function_completion.sh
@@ -3,14 +3,14 @@
 _bash_function_completion()
 {
   file_called="${COMP_WORDS[0]}"
- 
+
   # make completion work with tilde, expand to home
   full_file=$(echo "$file_called" | sed -E "s|^\~|${HOME}|")
 
   # get function names regardless of style: 'function foo {' or 'foo() {'
   function_names=`grep -oP 'function.*|.*\(\)' "$full_file" |sed -E 's/(function\s+)|(\s+\{)|(\(.*)//g'`
 
-  COMPREPLY=($(compgen -W "$function_names" -- "${COMP_WORDS[1]}"))
+  COMPREPLY=($(compgen -W "$function_names" -- "${COMP_WORDS[$COMP_CWORD]}"))
 }
 
 # To make files autocomplete, put a line like this somewhere it will be sourced (and source this file after)
