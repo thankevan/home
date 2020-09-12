7e9c6111224df35d7daf1c9781ac11b062ac83fc
thankevan <>
2020-09-11 22:50:30 -0400

    powerline check

--- bin/home_setup.sh
+++ bin/home_setup.sh
@@ -266,6 +266,35 @@ function mac_default_to_bash {
   chsh -s /bin/bash
 }
 
+function check_for_powerline_fonts {
+  echo ""
+  echo "POWERLINE"
+  echo "---------"
+
+  if [ "$POWERLINE_INSTALLED" == 1 ]; then
+    echo "powerline set as installed"
+    return 0
+  fi
+
+  echo -e "Powerline glyphs:\n\
+    Code points Glyphe  Description                Old code point
+    U+E0A0      \xee\x82\xa0       Version control branch     (U+2B60 \xe2\xad\xa0 )\n\
+    U+E0A1      \xee\x82\xa1       LN (line) symbol           (U+2B61 \xe2\xad\xa1 )\n\
+    U+E0A2      \xee\x82\xa2       Closed padlock             (U+2B64 \xe2\xad\xa4 )\n\
+    U+E0B0      \xee\x82\xb0       Rightwards black arrowhead (U+2B80 \xe2\xae\x80 )\n\
+    U+E0B1      \xee\x82\xb1       Rightwards arrowhead       (U+2B81 \xe2\xae\x81 )\n\
+    U+E0B2      \xee\x82\xb2       Leftwards black arrowhead  (U+2B82 \xe2\xae\x82 )\n\
+    U+E0B3      \xee\x82\xb3       Leftwards arrowhead        (U+2B83 \xe2\xae\x83 )\n\
+    "
+  read -p "Do you see symbols above? (y)" IS_POWERLINE_INSTALLED
+
+  if [ "$IS_POWERLINE_INSTALLED" == "y" ]; then
+    echo "export POWERLINE_INSTALLED=1" >> .bash_precustom
+  else
+    echo "Check out https://github.com/ryanoasis/nerd-fonts"
+  fi
+}
+
 function run_all {
   check_if_home_directory
   mac_setup
@@ -274,6 +303,7 @@ function run_all {
   backup_bash_files
   clone_home
   setup_custom_dot_files
+  check_for_powerline_fonts
   setup_git_global_configs
 }
 
