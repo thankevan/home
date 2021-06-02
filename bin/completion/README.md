841b77294af3c209467fdf15ece1e8ba785dc6b3
thankevan <>
2021-06-02 15:20:47 -0400

    Add docker completion and readme

---
+++ bin/completion/README.md
@@ -0,0 +1,5 @@
+
+git-completion.bash from: https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+docker-completion.sh from: https://github.com/docker/docker-ce/blob/master/components/cli/contrib/completion/bash/docker
+docker-compose-completion.sh from: https://docs.docker.com/compose/completion/
+
--- bin/completion/docker_script_completion.sh
+++ bin/completion/docker_script_completion.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CONTAINER_NAME_COMPLETE_SCRIPTS=(
-  docker_tail_logs_pretty.sh 
-  docker_rebuild_container.sh 
-  docker_run_command_on_nonrunning_container.sh 
-  docker_run_command_on_running_container.sh 
+  docker_tail_logs_pretty.sh
+  docker_rebuild_container.sh
+  docker_run_command_on_nonrunning_container.sh
+  docker_run_command_on_running_container.sh
   docker_run_command_on_running_container_as_root.sh
 )
 
@@ -19,7 +19,7 @@ _image_name_completion()
 {
   #docker images --filter "dangling=false" --format "{{.Repository}} {{.ID}}"
   image_names=`docker images --filter "dangling=false" --format "{{.Repository}}"`
-  
+
   COMPREPLY=($(compgen -W "$image_names" -- "${COMP_WORDS[$COMP_CWORD]}"))
 }
 
