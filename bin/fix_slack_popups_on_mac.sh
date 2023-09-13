8a2415bdc3bc39ebf03231a399d208d40bedf11b
thankevan <thankevan@gmail.com>
2023-09-13 16:06:29 -0400

    Fix slack popups improvements

--- bin/fix_slack_popups_on_mac.sh
+++ bin/fix_slack_popups_on_mac.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+
+echo
+echo "Correct user?"
+echo '$USER:staff' "=> $USER:staff"
+
+echo
+echo "Current ownership:"
+ls -al -G /Applications/Slack.app
+
+echo
+echo "Make the switch"
 sudo chown -R $USER:staff /Applications/Slack.app
+
+echo
+echo "Now:"
+ls -al -G /Applications/Slack.app
+
